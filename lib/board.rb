8aae7189526d9374a835754272a0957f2bde1ba7
Craig Donavin <crdonavin@gmail.com>
2019-06-05 00:52:04 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -10,11 +10,7 @@ class Board
   end
 
   def display
-    puts "#{cells[0]} | #{cells[1]} | #{cells[2]}
-          -----------
-          #{cells[3]} | #{cells[4]} | #{cells[5]}
-          -----------
-          #{cells[6]} | #{cells[7]} | #{cells[8]}"
+    puts "#{cells[0]} | #{cells[1]} | #{cells[2]} ----------- #{cells[3]} | #{cells[4]} | #{cells[5} ----------- #{cells[6]} | #{cells[7]} | #{cells[8]}"
   end
 
 end
